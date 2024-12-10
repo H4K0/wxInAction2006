26ca316fa3ea813b8a2031214058d6d67bdfeec7
H4K0 <jacoeche@gmail.com>
2024-12-10 11:56:56 -0500

    Examples PNG image manipulator

--- 004.2-hello.py
+++ 004.2-hello.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
 
 import wx
+
+"""
+Hello, wxPython! program. For PNG image.
+"""
+
+class ImageFrame(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        super(ImageFrame, self).__init__(*args, **kwargs)
+        self.image = wx.Image("src/splash.png", wx.BITMAP_TYPE_PNG) # Load the PNG image
+        self.bitmap = wx.StaticBitmap(self, bitmap=self.image.ConvertToBitmap()) # Create a StaticBitmap to display the image
+        self.SetSize(self.image.GetSize()) # Set the frame size to the size of the image
+        self.SetTitle("PNG Image Display") # Set the frame title
+        
+        self.Centre() # Center the window on the screen
+        self.Show() # Show the frame
+
+def main():
+    app = wx.App(False)  # Create a new application
+    frame = ImageFrame(None)  # Create a frame instance
+    app.MainLoop()  # Start the event loop
+
+if __name__ == "__main__":
+    main()
