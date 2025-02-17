da488c921a59fb1cd10ef9e3a10ce62398512e5f
H4K0 <jacoeche@gmail.com>
2025-02-17 02:42:22 -0500

    Implement output to sheet2 at spreadsheet

--- AttendanceApp/Ejemplo/proofsNnotes.py
+++ AttendanceApp/Ejemplo/proofsNnotes.py
@@ -3,3 +3,13 @@
 n = 0
 estado = "Positivo" if n > 0 else "Negativo" if n < 0 else "Cero"
 print(estado)
+
+
+
+celdas_en_negrita = hoja_tres["A1":"D1"]
+fuente = Font(bold=True)
+
+
+for row in celdas_en_negrita:
+    for cell in row:
+        cell.font = fuente
